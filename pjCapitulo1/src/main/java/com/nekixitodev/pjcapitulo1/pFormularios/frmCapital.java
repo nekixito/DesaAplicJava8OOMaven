fead7eb1a6cae1f016b1dd3eff7a8a218602ff1e
Miguel Nequiz <nekixito@gmail.com>
2024-02-17 12:36:04 -0600

    Se agregan comentarios de separación

--- pjCapitulo1/src/main/java/com/nekixitodev/pjcapitulo1/pFormularios/frmCapital.java
+++ pjCapitulo1/src/main/java/com/nekixitodev/pjcapitulo1/pFormularios/frmCapital.java
@@ -214,10 +214,15 @@ public class frmCapital extends javax.swing.JFrame {
     }//GEN-LAST:event_txtMonto3ActionPerformed
 
     private void btnCalcularActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCalcularActionPerformed
+        
+        //ENTRADA
+        
         //Recuperando información del formulario
         double monto1 = Double.parseDouble(txtMonto1.getText());
         double monto2 = Double.parseDouble(txtMonto2.getText());
         double monto3 = Double.parseDouble(txtMonto3.getText());
+        
+        //PROCESO
         
         //Calculando el monto del capital
         double capital = monto1 + monto2 + monto3;
@@ -226,6 +231,8 @@ public class frmCapital extends javax.swing.JFrame {
         double porcentaje1 = (monto1 * 100) / capital;
         double porcentaje2 = (monto2 * 100) / capital;
         double porcentaje3 = (monto3 * 100) / capital;
+        
+        //SALIDA
         
         //Imprimir los resultados
         lblCapital.setText("$ " + String.format("%.2f", capital));
