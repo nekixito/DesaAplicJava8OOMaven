9b68252b75f1db4f24c22d141a97ee5c44e3af59
Miguel Nequiz <nekixito@gmail.com>
2024-02-17 22:07:21 -0600

    Inicio de frmCapital en consola

---
+++ pjCapitulo1/src/main/java/com/nekixitodev/pjcapitulo1/pFormularios/frmCapitalConsola.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.nekixitodev.pjcapitulo1.pFormularios;
+
+/**
+ *
+ * @author nekix
+ */
+public class frmCapitalConsola {
+    public static void main(String[] args) {
+        
+    }
+}
